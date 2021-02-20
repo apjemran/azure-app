386ce788bad0f653a115dac53c9158ddadd2b39d
apjemran <apj.emran@gmail.com>
2021-02-21 00:19:16 +0530

    Updated web app

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ const http = require('http');
 const port = process.env.PORT || 3000;
 
 http.createServer(function(request, response) {
-  response.writeHead(200, { 'Content-Type': 'text/plain' });
-  response.end('Hello World!');
+  response.writeHead(200, { 'Content-Type': 'application/json' });
+  response.end('Hello Mohd Emran Khan !');
 }).listen(port);
 
 console.log(`Server running at http://localhost:${port}`);
