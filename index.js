a45f6b3c239479f90a6d7aa4fe46d22b6fa4e9db
apjemran <apj.emran@gmail.com>
2021-04-20 20:57:30 +0530

    Update index.js
    Test Azure App Service build trigger

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const port = process.env.PORT || 3000;
 
 http.createServer(function(request, response) {
   response.writeHead(200, { 'Content-Type': 'application/json' });
-  response.end('Hello Mohd Emran Khan !');
+  response.end('Hello Mohd Emran Khan ! How are you?');
 }).listen(port);
 
 console.log(`Server running at http://localhost:${port}`);
